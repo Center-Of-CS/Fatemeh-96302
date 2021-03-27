7c81f300c50924af10ae134baff8a80809d59d1c
FatemehRezwani <57309847+FatemehRezwani@users.noreply.github.com>
2021-03-27 15:00:25 +0430

    Add files via upload
    My project with php , work on CRUD function

---
+++ header.php
@@ -0,0 +1,28 @@
+<div class="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2 col-sm-3 col-xs-12"> <a href="index-2.html" class="logo"><img src="images/logo.png" alt="" /></a>
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+      <div class="col-md-10 col-sm-12 col-xs-12"> 
+        <!-- Nav start -->
+        <div class="navbar navbar-default" role="navigation">
+          <div class="navbar-collapse collapse" id="nav-main">
+            <ul class="nav navbar-nav">
+              <li><a href="allProduct.php"><i class="fa fa-plus-square"></i> Ads</a></li>
+              <li class="postad"><a href="index.php">Post an Ad</a></li>
+            </ul>
+            <!-- Nav collapes end --> 
+          </div>
+          <div class="clearfix"></div>
+        </div>
+        <!-- Nav end --> 
+      </div>
+    </div>
+    <!-- row end --> 
+  </div>
+  <!-- Header container end --> 
+</div>
